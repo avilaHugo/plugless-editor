fca5642061c775ce0c11d675a6eccac35eb19452
Hugo L. Ávila <53014804+avilaHugo@users.noreply.github.com>
2023-11-20 16:03:01 -0300

    Add a trick to decide if comments.sh should comment or uncomment lines.

--- scripts/comments.sh
+++ scripts/comments.sh
@@ -3,7 +3,9 @@
 comments::usage (){
     local __doc__
     read -r -d '' __doc__ <<-'EOM'
-        Comment or Uncomment lines from stdin
+    Comment or uncomment lines from stdin.
+    If the count of commented lines is greater than the uncommented ones,
+    the script will comment them; otherwise, it will uncomment them.
 
         Usage:
             comments.sh <COMMENT_STRING> < lines_to_comment.txt 
@@ -16,29 +18,46 @@ EOM
 }
 
 comments::main() {
+    # Argument variables
     local comment_string="${1}"
+    
+    # Loop variables 
     local line
-    local line_prefix
-    local added_comment
-    local line_suffix
+    local i
 
+    # Body variables 
+    local added_comment="${comment_string} "
+    local is_already_commented=0
+    declare -a line_prefixes
+    declare -a line_prexisting_comments
+    declare -a line_suffixes
+
+    # Break lines intro <PREFIX> <COMMENT(if any)> <SUFFIX>
     while IFS= read -r line; do
         
         [[ "${line}" =~ ^([' '$'\t']+)?("${comment_string}"' '+?)?(.*) ]]
 
-        line_prefix="${BASH_REMATCH[1]}"
-        line_suffix="${BASH_REMATCH[3]}"
-
-        [[ "${#BASH_REMATCH[2]}" -eq 0 ]] \
-            && added_comment="${comment_string} " \
-            || added_comment=''
-
-        printf '%s%s%s\n' \
-            "${line_prefix}" \
-            "${added_comment}" \
-            "${line_suffix}"
+        [[ ! "${#BASH_REMATCH[2]}" -eq 0 ]] && ((is_already_commented++))
+	
+	line_prefixes+=( "${BASH_REMATCH[1]}" )
+	line_prexisting_comments+=( "${BASH_REMATCH[2]}" )
+	line_suffixes+=( "${BASH_REMATCH[3]}" )
         
-    done 
+    done
+
+    # Count lines that are already commented
+    # to decide if comment or uncomment.
+    if [[ "${is_already_commented}" -eq "${#line_prefixes[@]}" ]];then
+	added_comment=''
+	line_prexisting_comments=()
+    fi
+
+    for (( i=0; i < "${#line_prefixes[@]}"; i++ ));do
+	printf '%s%s%s\n' \
+	       "${line_prefixes[$i]}" \
+	       "${added_comment}${line_prexisting_comments[$i]}" \
+	       "${line_suffixes[$i]}" 
+    done
 }
 
 [[ ! "${#1}" -eq 0  ]] \
